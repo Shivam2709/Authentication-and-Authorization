afb438225c433898d93b3459344615f0a4a0f2e6
Shivam2709 <shivamthakur2709@gmail.com>
2023-07-22 16:06:37 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Hello <span>Shivam</span> How are you?</h3>
+        <p>shivam@google.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
